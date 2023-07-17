fbe27f24611748257b80db17da1ffd0a508e102d
moses-ka <ji.moses.ka@gmail.com>
2023-07-17 21:25:32 +0200

    send the json file working =)

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const pool = require('./db')
-
+const  countries = require('./countries.json')
 
 // const route = require('./route')
 
-// const getCountry = (req, res) => {
-//     // res.send('hello there' )
-//     pool.query('SELECT * FROM countries ', (err, result) => {
-//         if (err) {
-//             console.log(err)
-//         }
-//         res.status(200).json(result)
+const getCountry = (req, res) => {
+    // res.send('hello there' )
+    pool.query('SELECT * FROM countries ', (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.status(200).send(countries)
         
-//     })
-// }
+    })
+}
 
 
 app.get('/', (req, res) => {
